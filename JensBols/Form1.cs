60d518fab821b7e39d05558270732068dd6959cb
Alimhan Milkiev <alimhanmilkiev@outlook.com>
2019-05-22 12:12:27 +0200

    Toevoeging knop en bericht door Alimhan

--- JensBols/Form1.cs
+++ JensBols/Form1.cs
@@ -16,5 +16,10 @@ namespace JensBols
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Alimhan eerste bericht");
+        }
     }
 }
