a2ae6b219c2fb7b365eabec790b9a7da0df9cc2b
John Galang <50907561+johngalang@users.noreply.github.com>
2019-05-29 11:35:54 +0200

    Update John
    Hello World

--- JensBols/Form1.cs
+++ JensBols/Form1.cs
@@ -19,7 +19,7 @@ namespace JensBols
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello Jens Bols!");
+            MessageBox.Show("Hello World");
             
         }
     }
